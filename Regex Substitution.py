56e1fd1ff6902d993e573da663289c58dbfcadfd
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-09-30 23:36:45 +0100

    Create Regex Substitution.py

---
+++ Regex Substitution.py
@@ -0,0 +1,54 @@
+"""
+Task:
+You are given a text of N lines. The text contains && and || symbols. 
+Your task is to modify those symbols to the following:
+&& → and
+|| → or
+Both && and || should have a space " " on both sides.
+
+Input Format:
+The first line contains the integer, N. 
+The next N lines each contain a line of the text.
+
+Neither && nor || occur in the start or end of each line.
+
+Output Format:
+Output the modified text.
+
+
+Sample Input
+11
+a = 1;
+b = input();
+
+if a + b > 0 && a - b < 0:
+    start()
+elif a*b > 10 || a/b < 1:
+    stop()
+print set(list(a)) | set(list(b)) 
+#Note do not change &&& or ||| or & or |
+#Only change those '&&' which have space on both sides.
+#Only change those '|| which have space on both sides.
+
+
+Sample Output
+a = 1;
+b = input();
+
+if a + b > 0 and a - b < 0:
+    start()
+elif a*b > 10 or a/b < 1:
+    stop()
+print set(list(a)) | set(list(b)) 
+#Note do not change &&& or ||| or & or |
+#Only change those '&&' which have space on both sides.
+#Only change those '|| which have space on both sides.    
+"""
+
+mport re
+
+for line in range(int(input())):
+    string = ''
+    string = re.sub(r'(?<= )&&(?= )','and',input())
+    string = re.sub(r'(?<= )\|\|(?= )','or',string)
+    print(string)
