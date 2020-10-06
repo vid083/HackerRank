ea14a6734c6aa5967462f52ce958f5d19329021a
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-10-06 23:34:51 +0100

    Create Check Subset.py

---
+++ Check Subset.py
@@ -0,0 +1,42 @@
+"""
+You are given two sets, A and B. 
+Your job is to find whether set A is a subset of set B.
+
+If set A is subset of set B, print True.
+If set A is not a subset of set B, print False.
+
+Input Format:
+The first line will contain the number of test cases,T . 
+The first line of each test case contains the number of elements in set A.
+The second line of each test case contains the space separated elements of set A.
+The third line of each test case contains the number of elements in set B.
+The fourth line of each test case contains the space separated elements of set B.
+
+Output Format:
+Output True or False for each test case on separate lines.
+
+Sample Input:
+3
+5
+1 2 3 5 6
+9
+9 8 5 6 3 2 1 4 7
+1
+2
+5
+3 6 5 4 1
+7
+1 2 3 5 6 8 9
+3
+9 8 2
+
+Sample Output:
+True 
+False
+False
+"""
+
+for i in range(int(input())):
+    _, a = input(), set(map(int, input().split()))
+    _, b = input(), set(map(int, input().split()))
+    print(a.issubset(b))
